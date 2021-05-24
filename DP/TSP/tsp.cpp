3fde846666ca95255c2d88193c80129a51711e9e
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2021-05-24 07:02:39 +0600

    Create tsp.cpp

---
+++ DP/TSP/tsp.cpp
@@ -0,0 +1,32 @@
+struct point{
+	int x = 0;
+	int y = 0;
+	point(int x = 0, int y = 0):x(x), y(y){}
+};
+inline int dist(point a, point b){
+	return abs(a.x - b.x) + abs(a.y - b.y);
+}
+int memo[1 << 11] [11];
+int tsp(){
+	vector<point> points;
+	for(int i = 0; i < (1 << n); i++){
+    	for(int j = 0; j < n; j++){
+    		memo[i] [j] = (int)1e5 + 7;
+    	}
+    }
+    memo[0] [0] = 0;
+    for(int mask = 0; mask < (1 << n); mask++){
+    	for(int prev = 0; prev < n; prev++){
+    		for(int i = 0; i < n; i++){
+    			if(!(mask & (1 << i))){
+    				memo[mask | (1 << i)] [i] = min(memo[mask | (1 << i)] [i], memo[mask] [prev] + dist(points[i], points[prev]));
+    			}
+    		}
+    	}
+    }
+    int ans = (int)1e5 + 7;
+    for(int i = 0; i < n; i++){
+    	ans = min(ans, memo[(1 << n) - 1] [i] + dist(points[i], points[0]));
+    }
+    return ans;
+}
