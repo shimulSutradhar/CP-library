e5f89e3e7710a43c401ee05a40b57f1830b66c31
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2021-05-27 09:03:02 +0600

    Update submask.cpp

--- DP/TSP/submask.cpp
+++ DP/TSP/submask.cpp
@@ -1,8 +1,8 @@
 vector<int> supermask[1 << n];
 for (int supMask=0; supMask < (1 << n); ++supMask){
-  for (int subMask = supMask; subMask; subMask = (subMask - 1) & supMask){
-    if(validSubMask[subMask]){
-      supermask[supMask].emplace_back(subMask);
+    for (int subMask = supMask; subMask; subMask = (subMask - 1) & supMask){
+        if(validSubMask[subMask]){
+            supermask[supMask].emplace_back(subMask);
+        }
     }
-  }
 }
