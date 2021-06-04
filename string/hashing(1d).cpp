a2026e263534aafe2a9399126f5edc2b3025943c
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2021-06-04 12:10:48 +0600

    Create hashing(1d).cpp

---
+++ string/hashing(1d).cpp
@@ -0,0 +1,83 @@
+// tested: uva 353 - Pesky Palindromes
+// hashing
+// getForwardHash(l...r)   = forwardHash[r] - forwardHash[l - 1] * base ^ (r - l + 1);
+// getBackwardHash(l...r)  = backwardHash[l] - backwardHash[r + 1] * base ^ (r - l + 1);
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long i64;
+const int MAX = 109;
+i64 mods[2] = {1000000007, 1000000009};
+//Some back-up primes: 1072857881, 1066517951, 1040160883
+i64 bases[2] = {137, 281};
+i64 powBase[2][MAX];
+void procedure(){
+    powBase[0] [0] = powBase[1] [0] = 1;
+    for(int i = 1; i < MAX; i++){
+        powBase[0] [i] = (powBase[0] [i - 1] * bases[0]) % mods[0];
+        powBase[1] [i] = (powBase[1] [i - 1] * bases[1]) % mods[1];
+    }
+}
+struct hashing{
+    i64 forwardHash[2] [MAX];
+    i64 backwardHash[2] [MAX];
+    string str;
+    int len;
+    hashing(string _str){
+        str = _str;
+        len = str.length() - 1;
+        memset(forwardHash, 0, sizeof forwardHash);
+        memset(backwardHash, 0, sizeof backwardHash);
+        forwardHashBuild();
+        backwardHashBuild();
+    }
+    // forward hash
+    void forwardHashBuild(){
+        forwardHash[0] [0] = str[0];
+        forwardHash[1] [0] = str[0];
+        for(int i = 1; i <= len; i++){
+            forwardHash[0] [i] = (forwardHash[0] [i - 1] * bases[0] + str[i]) % mods[0];
+            forwardHash[1] [i] = (forwardHash[1] [i - 1] * bases[1] + str[i]) % mods[1];
+        }
+    }
+    pair<i64, i64> getForwardHash(int left, int right){
+        assert(left <= right);
+        i64 ans[2];
+        for(int i = 0; i < 2; i++){
+            ans[i] = forwardHash[i] [right];
+            if(left > 0){
+                ans[i] -= (forwardHash[i] [left - 1] * powBase[i] [right - left + 1]) % mods[i];
+                if(ans[i] < 0){
+                    ans[i] += mods[i];
+                }
+            }
+        }
+        return {ans[0], ans[1]};
+    }
+    // backward hash
+    void backwardHashBuild(){
+        backwardHash[0] [len] = str[len];
+        backwardHash[1] [len] = str[len];
+        for(int i = len - 1; i >= 0; i--){
+            backwardHash[0] [i] = (backwardHash[0] [i + 1] * bases[0] + str[i]) % mods[0];
+            backwardHash[1] [i] = (backwardHash[1] [i + 1] * bases[1] + str[i]) % mods[1];
+        }
+    }
+    pair<i64, i64> getBackwardHash(int left, int right){
+        assert(left <= right);
+        i64 ans[2];
+        for(int i = 0; i < 2; i++){
+            ans[i] = backwardHash[i] [left];
+            if(right < len){
+                ans[i] -= (backwardHash[i] [right + 1] * powBase[i] [right - left + 1]) % mods[i];
+                if(ans[i] < 0){
+                    ans[i] += mods[i];
+                }
+            }
+        }
+        return {ans[0], ans[1]};
+    }
+};
+int main(){
+    procedure();
+    return 0;
+}
