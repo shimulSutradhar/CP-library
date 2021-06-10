d632a1d341da389c79a1a217d92b22b6c345e076
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2021-06-10 15:40:54 +0600

    Update trie_iterative.cpp

--- string/trie_iterative.cpp
+++ string/trie_iterative.cpp
@@ -1,15 +1,18 @@
 const int ALPHABET = 26;
 const int MAX = 1e6 + 7;
-int trie[MAX][ALPHABET], cnt[MAX];
+int trie[MAX][ALPHABET];
+bool isEnd[MAX];
 int node;
 void insert(string a){
     int now = 0;
     for(int i = 0; a[i]; i++){
         if(trie[now] [cmap[a[i]]] == -1){
             trie[now] [cmap[a[i]]] = ++node;
+            for(int j = 0; j < ALPHABET; j++){
+                trie[node] [i] = -1;
+            }
         }
         now = trie[now] [cmap[a[i]]];
-        ++cnt[now];
-        ans = max(ans, cnt[now] * (i + 1));
     }
+    isEnd[now] = true;
 }
