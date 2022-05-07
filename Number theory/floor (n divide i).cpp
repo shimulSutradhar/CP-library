a4eb23d5007b42a0956d4df8579f9116c82e6bfa
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2022-05-07 16:04:43 +0600

    Update floor (n divide i).cpp

--- Number theory/floor (n divide i).cpp
+++ Number theory/floor (n divide i).cpp
@@ -10,15 +10,15 @@ int main( ) {
   	scanf("%lld", &n);
   	i64 ans = 0;
   	for (i64 i = 1; i * i <= n; i++) {
-  		i64 lo = n / i;
-  		i64 hi = n / (i + 1);
+  		i64 hi = n / i;
+  		i64 lo = n / (i + 1);
   		// here i quotient (hi - lo) is frequency of i in floors sum(n / i)
-  		ans += i * (lo - hi);
+  		ans += i * (hi - lo);
   		i64 j = n / i;
   		if (i * i < n && j > i) {
-  			lo = n / j;
-  			hi = n / (j + 1);
-  			ans += j * (lo - hi);
+  			hi = n / j;
+  			lo = n / (j + 1);
+  			ans += j * (hi - lo);
   		}
   	}
   	printf("%lld\n", ans);
