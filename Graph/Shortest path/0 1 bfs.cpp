bc8e353acaf3966d8a8f35f8190b1999c2a3ba00
Shimul Sutradhar <56151176+shimulSutradhar@users.noreply.github.com>
2022-05-18 03:20:56 +0600

    Create 0 1 bfs.cpp

---
+++ Graph/Shortest path/0 1 bfs.cpp
@@ -0,0 +1,18 @@
+vector<long long> dist(n + 7, INT_MAX);
+deque<int> Q;
+Q.push_front(1);
+dist[1] = 0;
+while(!Q.empty()){
+  int u = Q.front();
+  Q.pop_front();
+  for(auto m : adj[u]){
+    if(dist[m.to] > m.weight + dist[u]){
+      dist[m.to] = m.weight + dist[u];
+      if(m.weight == 1){
+        Q.push_back(m.to);
+      }else{
+        Q.push_front(m.to);
+      }
+    }
+  }
+}
